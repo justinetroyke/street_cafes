d667a0e007dc582fb3e0223b7dc2c5e9ed7562b6
Justine <jjtroyke@gmail.com>
2020-03-01 16:13:08 -0700

    Added category to cafe table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20200301214233) do
+ActiveRecord::Schema.define(version: 20200301230716) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20200301214233) do
     t.string "address"
     t.string "post_code"
     t.integer "number_of_chairs"
+    t.string "category"
   end
 
 end
