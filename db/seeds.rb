662bd37d43e38e43be93c02149879c2364c20456
Justine <jjtroyke@gmail.com>
2020-03-01 15:59:55 -0700

    Seeded db with CSV

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,19 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+require 'csv'
+
+csv_text = File.read(Rails.root.join('db', 'seeds', 'Street_Cafes_2015_16.csv'))
+csv = CSV.parse(csv_text, :headers => true, :encoding => 'ISO-8859-1')
+csv.each do |row|
+  t = Cafe.new
+  t.name = row['Cafe/Restaurant Name']
+  t.address = row['Street Address']
+  t.post_code = row['Post Code']
+  t.number_of_chairs = row['Number of Chairs']
+  t.save
+  puts "#{t.name}, #{t.address} saved"
+end
+
+puts "There are now #{Model.count} rows in the cafes table"
