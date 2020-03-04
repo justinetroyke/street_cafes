15624c711732958a4ee5ef89887f49b807c99260
Justine <jjtroyke@gmail.com>
2020-03-03 17:15:45 -0700

    Created test for cafe model to return post codes

--- spec/models/cafe_spec.rb
+++ spec/models/cafe_spec.rb
@@ -8,4 +8,16 @@ describe Cafe, type: :model do
     it { should validate_presence_of(:number_of_chairs) }
     it { should validate_presence_of(:category) }
   end
+
+    describe 'methods' do
+    it 'it sorts cafes by post codes and list number of cafes and chairs in each' do
+      cafe1 = Cafe.new(name: 'All Bar One', address: '27 East Parade', post_code: 'LS1 5BN', number_of_chairs: 20)
+      cafe2 = Cafe.new(name: 'All Bar Two', address: '28 East Parade', post_code: 'AS1 5BN', number_of_chairs: 10)
+      cafe3 = Cafe.new(name: 'All Bar Three', address: '29 East Parade', post_code: 'AS1 5BN', number_of_chairs: 40)
+
+      post_codes = Cafe.post_codes
+
+      expect(post_codes).to eq([{'LS1 5BN': 1, number_of_chairs: 20}, {'AS1 5BN': 2, number_of_chairs: 50}])
+    end
+  end
 end
